c835199e46ea17c45c26ad253e698b73f5fd008f
Soup <87945079+Numb11@users.noreply.github.com>
2024-04-06 11:37:36 +0100

    File structure changes

--- test.py
+++
@@ -1,8 +0,0 @@
-def Get_Date_Prefix(date):
-        Prefixes = {1:"st", 2:"nd", 3:"rd"}
-        if date % 10 in Prefixes.keys():
-                return Prefixes[date%10]
-        else:
-                return "th"
-
-print(Get_Date_Prefix(31))
